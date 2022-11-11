397aef87f4fd1bb12282fbaefb93eba7dcec634b
mepux <patrick.meier@gmx.net>
2022-11-11 13:31:28 +0100

    Enhanced ICAP mode

--- src/main/java/com/github/toolarium/icap/client/dto/ICAPMode.java
+++ src/main/java/com/github/toolarium/icap/client/dto/ICAPMode.java
@@ -12,7 +12,28 @@ package com.github.toolarium.icap.client.dto;
  * @author Patrick Meier
  */
 public enum ICAPMode {
-    RESPMOD,
-    REQMOD,
-    FILEMOD;
+    RESPMOD("res"),
+    REQMOD("req"),
+    FILEMOD("file");
+    
+    private String tag;
+    
+    /**
+     * Constructor for ICAPMode
+     *
+     * @param tag the tag
+     */
+    ICAPMode(String tag) {
+        this.tag = tag;
+    }
+
+    
+    /**
+     * Get the tag 
+     *
+     * @return the tag
+     */
+    public String getTag() {
+        return tag;
+    }
 }
